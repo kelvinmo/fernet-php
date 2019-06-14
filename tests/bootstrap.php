5ebb0ba382f6f5f604af5077da1a7a37e2972a0c
Kelvin Mo <kelvinmo@users.noreply.github.com>
2019-06-14 21:31:38 +1000

    Add class aliasing code for phpunit compatibility

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -21,4 +21,10 @@ spl_autoload_register(function ($class) {
     return;
 });
 
+// PHPUnit 6 introduced a breaking change that
+// removed PHPUnit_Framework_TestCase as a base class,
+// and replaced it with \PHPUnit\Framework\TestCase
+if (!class_exists('\PHPUnit_Framework_TestCase') && class_exists('\PHPUnit\Framework\TestCase'))
+    class_alias('\PHPUnit\Framework\TestCase', '\PHPUnit_Framework_TestCase');
+
 ?>
